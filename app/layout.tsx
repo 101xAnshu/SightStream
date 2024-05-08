362ccdb38520f7096baf434d8ad737748c8bd122
Anshu2520 <anshuswn10@gmail.com>
2024-05-08 22:28:45 +0530

    Feat: Initial UI

---
+++ app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Sight Stream",
+  description:
+    "Streamline meetings effortlessly with Sight-Stream, the ultimate productivity companion.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <ClerkProvider
+        appearance={{
+          layout: {
+            logoImageUrl: "/icons/yoom-logo.png",
+            socialButtonsVariant: "iconButton",
+          },
+          variables: {
+            colorText: "#fff",
+            colorPrimary: "#0E78F9",
+            colorBackground: "#1C1f2E",
+            colorInputBackground: "#252a41",
+            colorInputText: "#fff",
+          },
+        }}
+      >
+        <body className={`${inter.className} bg-dark-2`}>
+          {children}
+          <Toaster />
+        </body>
+      </ClerkProvider>
+    </html>
+  );
+}

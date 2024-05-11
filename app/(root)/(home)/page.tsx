363de91865ce9fa6cc125edf6657a55e0cdb1fa5
Anshu2520 <anshuswn10@gmail.com>
2024-05-11 12:27:48 +0530

    fix:  time

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,5 @@
 import MeetingTypeList from "@/components/MeetingTypeList";
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
   const now = new Date();
@@ -16,7 +16,7 @@ const Home = () => {
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover bg-right bg-no-repeat">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
-            Upcoming Meeting at: 12:30 PM
+            Upcoming Meeting at 12:30 AM
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
